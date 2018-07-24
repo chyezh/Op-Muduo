27f80db28f6f857cb63ea1307fdf560cebbd8eca
chyezh <chyezh@outlook.com>
2018-07-24 13:56:52 +0800

    add socket

---
+++ httpack/network/socket.cc
@@ -0,0 +1,57 @@
+#include "socket.h"
+#include <unistd.h>
+
+CYZPP_BEGIN
+
+class InternetAddress;
+
+Socket::Socket(sock_domain_t domain, sock_type_t type, sock_proto_t protocol) {
+  sockfd_ = socket(domain, type, protocol);
+}
+
+Socket::Socket(Socket &&sock) : sockfd_(std::move(sock.sockfd_)) {
+  sock.sockfd_ = -1;
+}
+
+Socket &Socket::operator=(Socket &&sock) {
+  if(this != &sock) {
+    sockfd_ = std::move(sock.sockfd_);
+    sock.sockfd_ = -1;
+  }
+  return *this;
+}
+
+void Socket::bind(const InternetAddress& address) {
+  int bind_result = 0;
+  switch(address.family()) {
+    case AF_INET : bind_result = ::bind(sockfd_, address.getNativeAddress(), sizeof(sockaddr_in)); break;
+    case AF_INET6 : bind_result = ::bind(sockfd_, address.getNativeAddress(), sizeof(sockaddr_in6)); break;
+  }
+  // error handle
+  if(bind_result < 0)
+    perror("Socket::bind#");
+}
+
+void Socket::listen() {
+  if(::listen(sockfd_, SOMAXCONN) < 0)
+    perror("Socket:listen#");
+}
+
+Socket Socket::accept(InternetAddress& address) {
+  socklen_t len = sizeof(InternetAddress::storage_t);
+  sockfd_t accept_result = ::accept(sockfd_, const_cast<struct sockaddr*>(address.getNativeAddress()), &len);
+  if(accept_result < 0)
+    perror("Socket::accept#");
+  return Socket(accept_result);
+}
+
+bool Socket::connect(const InternetAddress& address) {
+  return ::connect(sockfd_, address.getNativeAddress(), sizeof(sockaddr_storage)) == 0;
+}
+
+void Socket::close() {
+  if(::close(sockfd_) < 0)
+    perror("Socket::close#");
+}
+
+CYZPP_END
---
+++ httpack/network/socket.h
@@ -0,0 +1,55 @@
+#ifndef _CYZPP_NETWORK_SOCKET_H__
+#define _CYZPP_NETWORK_SOCKET_H__
+
+#include "common.h"
+#include <sys/socket.h>
+#include "internet_address.h"
+
+CYZPP_BEGIN
+
+class InternetAddress;
+
+// move-only class
+class Socket {
+ public:
+  typedef int sockfd_t;
+  typedef int sock_proto_t;
+  typedef int sock_type_t;
+  typedef int sock_domain_t;
+
+  // constructor
+  Socket() : sockfd_(-1) {}
+
+  explicit Socket(sockfd_t fd) : sockfd_(fd) {}
+
+  Socket(sock_domain_t domain, sock_type_t type, sock_proto_t protocol);
+
+  Socket(const Socket &) = delete;
+
+  Socket(Socket && sock);
+
+  Socket &operator=(const Socket &) = delete;
+
+  Socket &operator=(Socket && sock);
+
+  bool valid() { return sockfd_ != -1; }
+
+  sockfd_t nativeHandler() const { return sockfd_; }
+
+  void bind(const InternetAddress& address);
+  
+  void listen();
+
+  Socket accept(InternetAddress& address);
+
+  bool connect(const InternetAddress& address);
+
+  void close();
+
+ private:
+  sockfd_t sockfd_;
+};
+
+CYZPP_END
+
+#endif // _CYZPP_NETWORK_SOCKET_H__
