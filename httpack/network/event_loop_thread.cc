2f40e2170ba2eb95620762b920ffb11e61e5c672
chyezh <chyezh@outlook.com>
2018-07-26 22:07:28 +0800

    add event_loop_thread

---
+++ httpack/network/event_loop_thread.cc
@@ -0,0 +1,39 @@
+#include "event_loop_thread.h"
+#include "event_loop.h"
+
+CYZPP_BEGIN
+
+EventLoop *EventLoopThread::event_loop_ = nullptr;
+
+std::thread EventLoopThread::event_thread_;
+
+std::mutex EventLoopThread::mutex_;
+
+std::condition_variable EventLoopThread::cond_;
+
+EventLoop *EventLoopThread::makeEventLoop() {
+  event_thread_ = std::thread(makeEventLoopFunc);
+  // wait event_loop_ return
+  std::unique_lock<std::mutex> lk(mutex_);
+  cond_.wait(lk, []() { return event_loop_ != nullptr; });
+  // recover event_loop_
+  EventLoop *new_event_loop = event_loop_;
+  event_loop_ = nullptr;
+  // detach the thread and return
+  event_thread_.detach();
+  return new_event_loop;
+}
+
+void EventLoopThread::makeEventLoopFunc() {
+  EventLoop loop;
+  {
+    // return new event_loop_
+    std::lock_guard<std::mutex> lk(mutex_);
+    event_loop_ = &loop;
+    cond_.notify_one();
+  }
+  // loop
+  loop.loop();
+}
+
+CYZPP_END
---
+++ httpack/network/event_loop_thread.h
@@ -0,0 +1,32 @@
+#ifndef _CYZPP_EVENT_LOOP_THREAD_H__
+#define _CYZPP_EVENT_LOOP_THREAD_H__
+
+#include "../common.h"
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+CYZPP_BEGIN
+
+class EventLoop;
+
+class EventLoopThread {
+ public:
+  EventLoopThread(const EventLoopThread &) = delete;
+
+  EventLoopThread &operator=(const EventLoopThread &) = delete;
+
+  static EventLoop* makeEventLoop();
+
+ private:
+  static void makeEventLoopFunc();
+  static EventLoop* event_loop_;
+  static std::thread event_thread_;
+  static std::mutex mutex_;
+  static std::condition_variable cond_;
+};
+
+CYZPP_END
+
+#endif // !_CYZPP_EVENT_LOOP_THREAD_H__
