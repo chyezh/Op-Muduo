5d3830b4a3a8d94512a0047ae41ee9027042d2f9
chyezh <chyezh@outlook.com>
2018-07-28 18:50:59 +0800

    update connnection

--- httpack/network/tcp_connection.cc
+++ httpack/network/tcp_connection.cc
@@ -1,23 +1,122 @@
 #include "tcp_connection.h"
+#include <unistd.h>
+#include <cassert>
+#include <algorithm>
+#include "channel.h"
+#include "event_loop.h"
 
 CYZPP_BEGIN
 
-TcpConnection::TcpConnection(TcpConnection &&connect)
-    : sock_(std::move(connect.sock_)),
-      peer_(sock_.nativeHandler(), InternetAddress::trans_opt::peer),
-      local_(sock_.nativeHandler(), InternetAddress::trans_opt::local) {
-  
+std::atomic_uint64_t TcpConnection::id_generator;
+
+TcpConnection::TcpConnectionID TcpConnection::generateID() {
+  return id_generator.fetch_add(1);
 }
 
-TcpConnection& TcpConnection::operator=(TcpConnection&& connect) {
-  if(this != &connect) {
-      sock_ = std::move(connect.sock_);
-      peer_ = peer_;
-      local_ = local_;
+TcpConnection::TcpConnection(EventLoop *loop, Socket &socket,
+                             const InternetAddress &address)
+    : owner_event_loop_(loop),
+      socket_(std::move(socket)),
+      channel_(std::make_unique<Channel>(socket_.nativeHandler(), loop)),
+      peer_(address),
+      id_(generateID()),
+      state_(Status::CONNECTED) {
+  channel_->setReadCallback([this]() { handleRead(); });
+  channel_->setWriteCallback([this]() { handleWrite(); });
+  channel_->setErrorCallback([this]() { handleRead(); });
+}
+
+void TcpConnection::connetionEstablish() {
+  owner_event_loop_->assertIsLoopingThread();
+  channel_->enableRead(true);
+  state_ = Status::ESTABLISHED;
+  established_callback_(shared_from_this());
+}
+
+void TcpConnection::setEstablishedCallback(const EstablishedCallback &call) {
+  established_callback_ = call;
+}
+
+void TcpConnection::setMessageCallback(const MessageCallback &call) {
+  message_callback_ = call;
+}
+
+void TcpConnection::setCloseCallback(const CloseCallback &call) {
+  close_callback_ = call;
+}
+
+size_t TcpConnection::recv(char *buffer, size_t len) {
+  return read_buffer_.read(static_cast<char *>(buffer), len);
+}
+
+size_t TcpConnection::recvNumber() { return read_buffer_.readableNumber(); }
+
+void TcpConnection::send(const char *buffer, size_t len) {
+  if (state_ == Status::ESTABLISHED) {
+    if (owner_event_loop_->isLoopingThread()) {
+      // run directly in io loop
+      sendInLoop(buffer, len);
+    } else {
+      // else, copy buffer and send to the io loop
+      owner_event_loop_->runInLoop(
+          [this, str = std::string(buffer, len)]() { sendInLoop(str); });
+    }
   }
-  return *this;
 }
 
+void TcpConnection::sendInLoop(const char *buffer, size_t len) {
+  owner_event_loop_->assertIsLoopingThread();
+  ssize_t write_num = 0;
+  if(!channel_->isWriting() && write_buffer_.readableNumber() == 0) {
+    write_num = ::write(socket_.nativeHandler(), buffer, len);
+  }
+  if(write_num != len) {
+    write_buffer_.write(buffer + write_num, len - write_num);
+  }
+  if(!channel_->isWriting() && write_buffer_.readableNumber() != 0) {
+    channel_->enableWrite(true);
+  }
+}
 
+void TcpConnection::sendInLoop(const std::string& str) {
+  owner_event_loop_->assertIsLoopingThread();
+  sendInLoop(str.data(), str.size());
+}
+
+void TcpConnection::handleRead() {
+  owner_event_loop_->assertIsLoopingThread();
+  assert(state_ == Status::ESTABLISHED);
+  // read the message into the buffer
+  ssize_t read_num = read_buffer_.readFrom(socket_.nativeHandler());
+  if (read_num > 0)
+    message_callback_(shared_from_this());
+  else if (read_num == 0)
+    handleClose();
+  else if (read_num < 0)
+    handleError();
+}
+
+void TcpConnection::handleWrite() {
+  owner_event_loop_->assertIsLoopingThread();
+  write_buffer_.writeTo(socket_.nativeHandler());
+  // if no data wait for sending, disable channel write signal
+  if(write_buffer_.readableNumber() == 0)
+    channel_->enableWrite(false);
+}
+
+void TcpConnection::handleError() {
+  owner_event_loop_->assertIsLoopingThread();
+}
+
+void TcpConnection::handleClose() {
+  owner_event_loop_->assertIsLoopingThread();
+  assert(state_ == Status::ESTABLISHED);
+  channel_->enableRead(false);
+  channel_->enableWrite(false);
+  channel_->removeSelf();
+  state_ = Status::DISCONNECTED;
+  socket_.close();
+  close_callback_(shared_from_this());
+}
 
 CYZPP_END
--- httpack/network/tcp_connection.h
+++ httpack/network/tcp_connection.h
@@ -1,27 +1,111 @@
 #ifndef _CYZPP_TCP_CONNECTION_H__
 #define _CYZPP_TCP_CONNECTION_H__
 
+#include "../common.h"
 #include "socket.h"
+#include "circular_buffer.h"
+#include <memory>
+#include <functional>
+#include <atomic>
+#include <list>
 
 CYZPP_BEGIN
 
-class TcpConnection {
+class Channel;
+
+class EventLoop;
+
+class CircularBuffer;
+
+class TcpConnection : public std::enable_shared_from_this<TcpConnection> {
  public:
-  TcpConnection();
+  typedef std::function<void(TcpConnectionPtr)> EstablishedCallback;
+  typedef std::function<void(TcpConnectionPtr)> MessageCallback;
+  typedef std::function<void(TcpConnectionPtr)> ErrorCallback;
+  typedef std::function<void(TcpConnectionPtr)> CloseCallback;
+  typedef uint64_t TcpConnectionID;
+
+  enum class Status {
+    CONNECTED,
+    ESTABLISHED,
+    DISCONNECTED
+  };
+
+  TcpConnection(EventLoop *loop, Socket &socket, const InternetAddress& address);
 
   TcpConnection(const TcpConnection&) = delete;
 
   TcpConnection& operator=(const TcpConnection&) = delete;
 
-  TcpConnection(TcpConnection &&connect);
+  uint64_t getID() const { return id_; }
 
-  TcpConnection& operator=(TcpConnection&& connect);
+  void connetionEstablish();
 
+  void setEstablishedCallback(const EstablishedCallback &call);
+
+  void setMessageCallback(const MessageCallback &call);
+
+  void setCloseCallback(const CloseCallback &call);
+
+  size_t recv(char *buffer, size_t len);
+
+  size_t recvNumber();
+
+  void send(const char* buffer, size_t len);
+
+  // for connection read callback
+  void handleRead();
+
+  // for channel write callback
+  void handleWrite();
+
+  // for channel error callback
+  void handleError();
+
+  // for close the connection
+  void handleClose();
+
+  // generate unique id for timer
+  static TcpConnectionID generateID();
 
  private:
-  Socket sock_;
+  void sendInLoop(const char* buffer, size_t len);
+
+  void sendInLoop(const std::string& str);
+
+  // owner loop
+  EventLoop* owner_event_loop_;
+  
+  // hold socket
+  Socket socket_;
+  
+  // envent distribution channel
+  std::unique_ptr<Channel> channel_;
+
+  // buffers
+  // read buffer
+  CircularBuffer read_buffer_;
+  // write buffer
+  CircularBuffer write_buffer_;
+
+  // call back
+  MessageCallback message_callback_;
+
+  EstablishedCallback established_callback_;
+  
+  CloseCallback close_callback_;
+  
+  // peer address
   InternetAddress peer_;
-  InternetAddress local_;
+
+  // unique id for every connection
+  uint64_t id_;
+
+  // state of this connection
+  Status state_;
+
+  // unique id generator
+  static std::atomic_uint64_t id_generator;
 };
 
 CYZPP_END
